919f0d4d51a5213373a939e39179fb4a46f2c660
unknown <willson.melo@labinfo.is.escuelaing.edu.co>
2020-02-14 10:25:15 -0500

    added to heroku and spark Api

--- src/main/java/edu/escuelaing/arep/App.java
+++ src/main/java/edu/escuelaing/arep/App.java
@@ -1,13 +1,31 @@
 package edu.escuelaing.arep;
 
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+import static spark.Spark.staticFiles;
+
+import com.google.gson.Gson;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public static void main(String[] args) {
+        port(getPort());
+        
+        get("/hello", (req, res) -> "Hello Heroku");
+       
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; // returns default port if heroku-port isn't set(i.e. on localhost)
     }
 }
